512a04f1f15cf535f85e14f34324f55f8625d9bc
MRthehax1sout <ggaamar8@gmail.com>
2025-01-30 20:38:44 +0200

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["⚖️ Committed to justice and safety", "👮 Upholding law and order", "🛡️ Serving and protecting"];
+const statusMessages = ["Faking the Moon Landing", "Making CGI Images", "Protecting the Ice Wall"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
